a527fb6bf8fcf47e4c4e2bf98d6ce90fc1368c9c
bhatrahuls <58146797+bhatrahuls@users.noreply.github.com>
2023-07-31 14:48:10 +0530

    Add files via upload

---
+++ Chess/src/main/java/org/example/Spots.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Spots {
+    private int x;
+    private int y;
+    private Piece piece;
+
+    public Spots(int x,int y, Piece piece){
+        this.setX(x);
+        this.setY(y);
+        this.setPiece(piece);
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+}
